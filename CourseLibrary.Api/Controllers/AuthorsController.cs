92444d6b50d4075777bdab378079f047d7d0c78d
atlit06 <atlithrastarson@gmail.com>
2020-04-13 14:37:13 +0000

    proj init plurarlsight rest api

---
+++ CourseLibrary.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+using System;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+        }
+    }
+}
